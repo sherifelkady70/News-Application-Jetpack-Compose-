3b699a833ddf5736ebd52817ca29a03676dc2b44
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-01 16:37:18 +0300

    create dataclass page to define page information.

--- app/src/main/java/com/route/newsapplication/presentation/onboarding/Page.kt
+++ app/src/main/java/com/route/newsapplication/presentation/onboarding/Page.kt
@@ -1,3 +1,18 @@
 package com.route.newsapplication.presentation.onboarding
 
-data class Page()
+import androidx.annotation.DrawableRes
+import com.route.newsapplication.R
+
+data class Page(
+    val title : String,
+    val description : String,
+    @DrawableRes val img : Int
+)
+
+
+val pages = listOf(
+    Page("","", R.drawable.onboarding1),
+    Page("","", R.drawable.onboarding2),
+    Page("","", R.drawable.onboarding3),
+
+    )
---
+++ app/src/main/java/com/route/newsapplication/presentation/onboarding/components/OnBoardingPage.kt
@@ -0,0 +1,13 @@
+package com.route.newsapplication.presentation.onboarding.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.route.newsapplication.presentation.onboarding.Page
+
+@Composable
+fun OnBoardingPage(
+    page : Page,
+    modifier : Modifier = Modifier
+) {
+
+}
