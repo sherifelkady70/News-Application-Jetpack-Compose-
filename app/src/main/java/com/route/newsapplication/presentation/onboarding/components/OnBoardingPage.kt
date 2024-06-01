11ca51dc753c7f49e2d72a7b8b87b1269eea3fea
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-01 17:03:17 +0300

    create onBoarding design .

--- app/src/main/java/com/route/newsapplication/presentation/onboarding/components/OnBoardingPage.kt
+++ app/src/main/java/com/route/newsapplication/presentation/onboarding/components/OnBoardingPage.kt
@@ -1,7 +1,25 @@
 package com.route.newsapplication.presentation.onboarding.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.route.newsapplication.R
 import com.route.newsapplication.presentation.onboarding.Page
 
 @Composable
@@ -9,5 +27,29 @@ fun OnBoardingPage(
     page : Page,
     modifier : Modifier = Modifier
 ) {
+    Column(modifier = modifier) {
+        Image(painterResource(id =page.img) , contentDescription = "image page",
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.6f),
+            contentScale = ContentScale.Crop)
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(text = page.title,
+            modifier = Modifier.padding(horizontal = 30.dp),
+            style = MaterialTheme.typography.bodySmall.copy(fontWeight = FontWeight.Bold),
+            color = colorResource(id = R.color.display_small))
 
+
+        Text(text = page.description,modifier = Modifier.padding(horizontal = 30.dp),
+            style = MaterialTheme.typography.bodyMedium,
+            color = colorResource(id = R.color.text_medium))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Display() {
+    OnBoardingPage(page = Page("this is first title ",
+        "ssdadasdasdasdasdasdasdasdasdasdasdasdasd"
+        ,R.drawable.onboarding1))
 }
