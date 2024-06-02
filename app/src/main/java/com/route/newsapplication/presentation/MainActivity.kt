9343c4f5c8eaf1f047ff191d43e5323e6cbc8841
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-02 18:00:20 +0300

    Run OnBoardingScreen.

--- app/src/main/java/com/route/newsapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/route/newsapplication/presentation/MainActivity.kt
@@ -3,14 +3,18 @@ package com.route.newsapplication.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.route.newsapplication.presentation.onboarding.OnBoardingScreen
 import com.route.newsapplication.ui.theme.NewsApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +22,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContent {
-            NewsApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            NewsApplicationTheme (
+                dynamicColor = false
+            ) {
+                Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)){
+                    OnBoardingScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NewsApplicationTheme {
-        Greeting("Android")
-    }
-}
