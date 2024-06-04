025773a844747b93705d1f3001005e7f3c1c9fd2
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-04 19:35:15 +0300

    Add text button in On Boarding Screen.

--- app/src/main/java/com/route/newsapplication/presentation/onboarding/OnBoardingPageScreen.kt
+++ app/src/main/java/com/route/newsapplication/presentation/onboarding/OnBoardingPageScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.route.newsapplication.presentation.onboarding.common.NewsButton
+import com.route.newsapplication.presentation.onboarding.common.NewsTextButton
 import com.route.newsapplication.presentation.onboarding.components.OnBoardingPage
 import com.route.newsapplication.presentation.onboarding.components.PageIndicator
 import kotlinx.coroutines.launch
@@ -55,15 +56,18 @@ fun OnBoardingScreen() {
                 .navigationBarsPadding(),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
-        ){
-            PageIndicator(modifier = Modifier.width(52.dp)
-                , pageSize = pages.size
-                , selectedPage = pagerState.currentPage)
-            Row(verticalAlignment = Alignment.CenterVertically){
+        ) {
+            PageIndicator(
+                modifier = Modifier.width(52.dp),
+                pageSize = pages.size,
+                selectedPage = pagerState.currentPage
+            )
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
                 val scope = rememberCoroutineScope()
 
-                if(buttonState.value[0].isNotEmpty()){
-                    NewsButton(text = buttonState.value[0]) {
+                if (buttonState.value[0].isNotEmpty()) {
+                    NewsTextButton(text = buttonState.value[0]) {
                         scope.launch {
                             pagerState.animateScrollToPage(
                                 page = pagerState.currentPage - 1
@@ -72,20 +76,21 @@ fun OnBoardingScreen() {
                     }
                 }
 
-                
+
                 NewsButton(text = buttonState.value[1]) {
-                   scope.launch {
-                       if (pagerState.currentPage == 3){
-                           //Navigate to the main screen and save a value in datastore preferences
-                       }else{
-                           pagerState.animateScrollToPage(
-                               page = pagerState.currentPage+1
-                           )
-                       }
-                   }
+                    scope.launch {
+                        if (pagerState.currentPage == 3) {
+                            //Navigate to the main screen and save a value in datastore preferences
+                        } else {
+                            pagerState.animateScrollToPage(
+                                page = pagerState.currentPage + 1
+                            )
+                        }
+                    }
                 }
             }
         }
+
         Spacer(modifier = Modifier.weight(0.5f))
     }
 }
--- app/src/main/java/com/route/newsapplication/presentation/onboarding/common/Button.kt
+++ app/src/main/java/com/route/newsapplication/presentation/onboarding/common/Button.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -27,4 +28,19 @@ fun NewsButton(
             style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold)
         )
     }
+}
+
+@Composable
+fun NewsTextButton(
+    text: String,
+    onCList: () -> Unit
+) {
+    TextButton(onClick = { /*TODO*/ }) {
+        Text(
+            text = text ,
+            style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold) ,
+            color = Color.LightGray
+        )
+    }
+
 }
