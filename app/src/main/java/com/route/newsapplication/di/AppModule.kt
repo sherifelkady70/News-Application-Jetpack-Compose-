a678904525ca572cdc996830ee23221e7bbda85c
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-04 21:09:09 +0300

    provide obj from local user manager .

--- app/src/main/java/com/route/newsapplication/di/AppModule.kt
+++ app/src/main/java/com/route/newsapplication/di/AppModule.kt
@@ -1,12 +1,22 @@
 package com.route.newsapplication.di
 
+import android.app.Application
+import com.route.newsapplication.ApplicationClass
+import com.route.newsapplication.data.manager.LocalUserManagerImpl
+import com.route.newsapplication.domain.manager.LocalUserManager
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
-
+    @Provides
+    @Singleton
+    fun provideLocalUserManager(application: Application) : LocalUserManager {
+        return LocalUserManagerImpl(application)
+    }
 }
