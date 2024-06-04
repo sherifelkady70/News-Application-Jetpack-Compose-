3441132f2de60e48ec49bc571ab78852fe115489
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-04 20:49:45 +0300

    create data store impl.

--- app/src/main/java/com/route/newsapplication/data/manager/LocalUserManagerImpl.kt
+++ app/src/main/java/com/route/newsapplication/data/manager/LocalUserManagerImpl.kt
@@ -3,21 +3,34 @@ package com.route.newsapplication.data.manager
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
 import com.route.newsapplication.domain.manager.LocalUserManager
+import com.route.newsapplication.util.Constants
+import com.route.newsapplication.util.Constants.APP_ENTRY
 import com.route.newsapplication.util.Constants.USER_SETTINGS
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class LocalUserManagerImpl (
     private val context : Context
 ): LocalUserManager {
     override suspend fun saveAppEntry() {
-        TODO("Not yet implemented")
+       context.dataStore.edit { settingsPreference ->
+           settingsPreference[PreferencesKey.APP_ENTRY] = true
+       }
     }
 
     override fun readAppEntry(): Flow<Boolean> {
-        TODO("Not yet implemented")
+        return context.dataStore.data.map {
+            it[PreferencesKey.APP_ENTRY] ?: false
+        }
     }
 }
 
 private val Context.dataStore : DataStore<Preferences> by preferencesDataStore(name = USER_SETTINGS)
+
+private object PreferencesKey {
+    val APP_ENTRY = booleanPreferencesKey(name = Constants.APP_ENTRY)
+}
--- app/src/main/java/com/route/newsapplication/util/Constants.kt
+++ app/src/main/java/com/route/newsapplication/util/Constants.kt
@@ -2,4 +2,5 @@ package com.route.newsapplication.util
 
 object Constants {
     val USER_SETTINGS = "userDataStore"
+    val APP_ENTRY = "App Entry"
 }
