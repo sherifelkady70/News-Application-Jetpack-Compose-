95aa0648cf0fd9f98d9b88d6852fc1f7ead9e746
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-05 17:39:20 +0300

    create Route sealed class for screens .

---
+++ app/src/main/java/com/route/newsapplication/presentation/navgraph/Route.kt
@@ -0,0 +1,14 @@
+package com.route.newsapplication.presentation.navgraph
+
+sealed class Route(
+    route : String
+) {
+    data object OnBoardingScreen : Route("onBoardingScreen")
+    data object HomeScreen : Route("homeScreen")
+    data object SearchScreen : Route("searchScreen")
+    data object BookMarkScreen : Route("bookMarkScreen")
+    data object DetailsScreen : Route("detailsScreen")
+    data object AppStartNavigation : Route("appStartNav")
+    data object NewsNavigation : Route("newsNavigation")
+    data object NewsNavigator : Route("newsNavigator")
+}
