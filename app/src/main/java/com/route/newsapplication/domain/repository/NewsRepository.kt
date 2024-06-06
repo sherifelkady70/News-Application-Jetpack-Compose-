21aca15b207321a48fb84d94d5b494a47f98d9b1
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-06 14:10:03 +0300

    create news repo interface in domain layer .

---
+++ app/src/main/java/com/route/newsapplication/domain/repository/NewsRepository.kt
@@ -0,0 +1,10 @@
+package com.route.newsapplication.domain.repository
+
+import androidx.paging.PagingData
+import com.route.newsapplication.domain.models.Article
+import com.route.newsapplication.domain.models.Source
+import kotlinx.coroutines.flow.Flow
+
+interface NewsRepository {
+    fun getNews(sources : List<Source>) : Flow<PagingData<Article>>
+}
