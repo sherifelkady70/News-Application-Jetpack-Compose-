0d99e3a552cbb7f9e63ab328459fe0398509875d
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-09 23:06:30 +0300

    finish search bar  .

--- app/src/main/java/com/route/newsapplication/presentation/common/SearchBar.kt
+++ app/src/main/java/com/route/newsapplication/presentation/common/SearchBar.kt
@@ -1,22 +1,37 @@
 package com.route.newsapplication.presentation.common
 
+import android.content.res.Configuration
 import androidx.compose.foundation.border
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.route.newsapplication.R
+import com.route.newsapplication.ui.theme.NewsApplicationTheme
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBar(
     modifier : Modifier = Modifier,
@@ -37,9 +52,47 @@ fun SearchBar(
     }
 
     Box(modifier = modifier){
-        TextField(modifier = Modifier.fillMaxWidth()
-            ,value = text
-            , onValueChange = onValueChange)
+        TextField(
+            modifier = Modifier.fillMaxWidth()
+                .searchBarBoarder(),
+            value = text,
+            onValueChange = onValueChange,
+            readOnly = readOnly,
+            leadingIcon =
+            {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_search), contentDescription = null,
+                    modifier = Modifier.size(20.dp),
+                    tint = colorResource(id = R.color.body)
+                )
+            },
+            placeholder = {
+                Text(
+                    text = "Search",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = colorResource(R.color.placeholder)
+                )
+            },
+            shape = MaterialTheme.shapes.medium,
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = colorResource(R.color.input_background),
+                focusedTextColor = if (isSystemInDarkTheme()) Color.White else Color.Black,
+                cursorColor = if (isSystemInDarkTheme()) Color.White else Color.Black,
+                focusedIndicatorColor = Color.Transparent,
+                errorIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onSearch()
+            }),
+            textStyle = MaterialTheme.typography.bodySmall,
+            interactionSource = interactionSource
+            )
+
     }
 }
 
@@ -53,5 +106,16 @@ fun Modifier.searchBarBoarder() = composed {
         )
     }else{
         this
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun previewSearchBar(){
+    NewsApplicationTheme {
+        SearchBar(text = " ", readOnly = false, onValueChange ={} ){
+
+        }
     }
 }
