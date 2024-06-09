f225af12b25321301683e24b3670800bdedf04bb
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-09 18:52:55 +0300

    Continue setup Home Screen File. setup search bar file .

--- app/src/main/java/com/route/newsapplication/presentation/common/SearchBar.kt
+++ app/src/main/java/com/route/newsapplication/presentation/common/SearchBar.kt
@@ -1,6 +1,13 @@
 package com.route.newsapplication.presentation.common
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
 
@@ -13,5 +20,19 @@ fun SearchBar(
     onValueChange : (String) -> Unit,
     onSearch : () -> Unit
 ){
+    val interactionSource = remember{
+        MutableInteractionSource()
+    }
+    val isClicked = interactionSource.collectIsPressedAsState().value
+    LaunchedEffect(key1 = isClicked) {
+        if(isClicked){
+            onClick?.invoke()
+        }
+    }
 
+    Box(modifier = modifier){
+        TextField(modifier = Modifier.fillMaxWidth()
+            ,value = text
+            , onValueChange = onValueChange)
+    }
 }
