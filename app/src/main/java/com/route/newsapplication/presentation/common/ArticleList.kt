05bf663173684845ca824795946ca4817073a9e2
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-09 18:20:57 +0300

    setup articles list file for display articles paging data.

--- app/src/main/java/com/route/newsapplication/presentation/common/ArticleList.kt
+++ app/src/main/java/com/route/newsapplication/presentation/common/ArticleList.kt
@@ -2,22 +2,41 @@ package com.route.newsapplication.presentation.common
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import com.route.newsapplication.domain.models.Article
+import com.route.newsapplication.presentation.home.components.ArticleCard
+import com.route.newsapplication.util.Constants.ExtraSmallPadding
 import com.route.newsapplication.util.Constants.MediumPadding1
 
 
 @Composable
 fun ArticleList(
     modifier: Modifier = Modifier,
-    articles : Article ,
+    articles : LazyPagingItems<Article> ,
     onClick : (Article) -> Unit
 ){
+    val handlePagingData = handlingPagingResult(articles = articles)
 
+    if(handlePagingData){
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(MediumPadding1),
+            contentPadding = PaddingValues(ExtraSmallPadding),
+        ) {
+            items(articles.itemCount) {
+                articles[it]?.let {
+                    ArticleCard(article = it, onClick = { onClick })
+                }
+            }
+        }
+    }
 }
 
 
