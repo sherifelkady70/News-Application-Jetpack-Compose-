793acdea3a37bf25c77a1fbe9a885ac94239a970
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-09 14:05:48 +0300

    setup articles list file for display articles paging data.

--- app/src/main/java/com/route/newsapplication/presentation/common/ArticleList.kt
+++ app/src/main/java/com/route/newsapplication/presentation/common/ArticleList.kt
@@ -1,8 +1,8 @@
 package com.route.newsapplication.presentation.common
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposeCompilerApi
 import androidx.compose.ui.Modifier
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import com.route.newsapplication.domain.models.Article
 
@@ -18,14 +18,22 @@ fun ArticleList(
 
 
 @Composable
-fun HandlingPagingResult(
+fun handlingPagingResult(
     articles: LazyPagingItems<Article>
-){
+) : Boolean{
+    val loadState = articles.loadState
+    val error = when {
+        loadState.refresh is LoadState.Error -> loadState.refresh as LoadState.Error
+        loadState.append is LoadState.Error -> loadState.append as LoadState.Error
+        loadState.prepend is LoadState.Error -> loadState.prepend as LoadState.Error
+        else -> null
+    }
+
 
 }
 
 
 @Composable
 fun ShimmerEffect(){
-
+    ArticleShimmerEffect()
 }
--- app/src/main/java/com/route/newsapplication/presentation/common/ShimmerEffect.kt
+++ app/src/main/java/com/route/newsapplication/presentation/common/ShimmerEffect.kt
@@ -6,38 +6,28 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.route.newsapplication.R
 import com.route.newsapplication.util.Constants.MediumPadding1
 
 @Composable
-fun Modifier.shimmerEffect() = composed {
+fun Modifier.shimmerEffect() : Modifier = composed {
     val transition = rememberInfiniteTransition()
     val alpha = transition.animateFloat(
         initialValue = 0.2f,
@@ -50,7 +40,7 @@ fun Modifier.shimmerEffect() = composed {
     background(colorResource(id = R.color.shimmer).copy(alpha))
 }
 
-
+@Preview(showBackground = true)
 @Composable
 fun ArticleShimmerEffect(
     modifier: Modifier = Modifier
