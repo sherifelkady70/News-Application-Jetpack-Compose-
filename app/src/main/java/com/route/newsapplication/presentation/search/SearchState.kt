60caecc7bd80cdae8630fc85285f7b8a6e2aacf4
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-10 23:38:30 +0300

    finish search view model   .

--- app/src/main/java/com/route/newsapplication/presentation/search/SearchState.kt
+++ app/src/main/java/com/route/newsapplication/presentation/search/SearchState.kt
@@ -2,8 +2,9 @@ package com.route.newsapplication.presentation.search
 
 import androidx.paging.PagingData
 import com.route.newsapplication.domain.models.Article
+import kotlinx.coroutines.flow.Flow
 
 data class SearchState(
     val search : String ="" ,
-    val articles : PagingData<Article>?=null
+    val articles : Flow<PagingData<Article>>?=null
 )
--- app/src/main/java/com/route/newsapplication/presentation/search/SearchViewModel.kt
+++ app/src/main/java/com/route/newsapplication/presentation/search/SearchViewModel.kt
@@ -3,6 +3,8 @@ package com.route.newsapplication.presentation.search
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.route.newsapplication.domain.usecase.news.NewsUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -18,11 +20,20 @@ class SearchViewModel @Inject constructor(
     fun onEvent(event : SearchEvent){
         when(event){
             is SearchEvent.UpdateSearchQuery ->{
-
+               _state.value = state.value.copy(search = event.search)
             }
             is SearchEvent.SearchNews -> {
-
+                searchNews()
             }
         }
+    }
+
+    private fun searchNews(){
+        val articles = searchUseCase.searchInNews(
+            search = state.value.search,
+            sources = listOf("bbc-news","abc-news","al-jazeera-english")
+        ).cachedIn(viewModelScope)
+
+        _state.value =  state.value.copy(articles = articles)
     }
 }
